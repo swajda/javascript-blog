d082e894b4362195fad9607722ed16f2340430f9
Szymon Wajda <wasz92@gmail.com>
2022-03-28 19:49:30 +0200

    Add showing clicked article

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 'use strict';
 
 function titleClickHandler(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
-
+    
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
     console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active')
     /* [DONE] remove class 'active' from all articles */
@@ -21,11 +22,14 @@ function titleClickHandler(event){
         activeArticle.classList.remove('active');
     }
     /* get 'href' attribute from the clicked link */
-  
+    const getHref = clickedElement.getAttribute('href');
+    console.log(getHref);
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    const targetArticle = document.querySelector(getHref);
+    console.log(targetArticle);
     /* add class 'active' to the correct article */
-  }
+    targetArticle.classList.add('active')
+}
   
   const links = document.querySelectorAll('.titles a');
   
